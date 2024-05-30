e07ca5f2aacd37f3a341837d4d9e79fb7d2a8f03
sanghonam <ska0294@gmail.com>
2024-05-30 19:13:19 +0900

    추가: mui, react-query 프로바이더 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ReactQueryClientProvider } from "./ReactQueryClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryClientProvider>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </ReactQueryClientProvider>
+      </body>
     </html>
   );
 }
